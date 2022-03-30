e30462e8833e8ede1ef6f1f1a3fad031015a6bd5
MKhodagholi <MKhodaagholi@gmail.com>
2022-03-30 10:54:41 +0430

    3. create Header Component and add into App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+import Home from "./components/Home/Home";
+
 import "./App.scss";
+import Header from "./components/Header/Header";
 
 const App = () => {
-  return <div className="app">Hello</div>;
+  return (
+    <div className="app">
+      <Header />
+      <Home />
+    </div>
+  );
 };
 
 export default App;
