f2500a364b880ad2975868e7e155f42b61416e87
MKhodagholi <MKhodaagholi@gmail.com>
2022-03-30 12:55:46 +0430

    6. add store for save data

---
+++ src/store.js
@@ -0,0 +1,10 @@
+export const bases = ["Classic", "Thin & Crispy", "Think Crust"];
+
+export const toppings = [
+  "mushrooms",
+  "pappers",
+  "onions",
+  "olives",
+  "extra chees",
+  "tomatoes",
+];
