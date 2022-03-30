757a7cda02cf5ff9cf904e84572448a6768ec411
MKhodagholi <MKhodaagholi@gmail.com>
2022-03-30 12:57:08 +0430

    9. add Routes and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Home from "./components/Home/Home";
 
 import "./App.scss";
 import Header from "./components/Header/Header";
+import Base from "./components/Base/Base";
+import { Switch, Route } from "react-router-dom";
 
 const App = () => {
+  const [pizza, setPizza] = useState({
+    base: null,
+    topping: null,
+  });
+
+  const addBase = (base) => {
+    setPizza((prev) => ({ ...prev, base }));
+  };
+
   return (
     <div className="app">
       <Header />
-      <Home />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/base">
+          <Base pizza={pizza} addBase={addBase} />
+        </Route>
+      </Switch>
     </div>
   );
 };
