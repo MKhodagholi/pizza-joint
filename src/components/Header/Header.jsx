eb4ee4ff1b49c7a0b11656b17350daca6202c8d1
MKhodagholi <MKhodaagholi@gmail.com>
2022-03-31 23:05:13 +0430

    24. add svg animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,21 +2,50 @@ import React from "react";
 
 import { motion } from "framer-motion";
 
+const svgVariant = {
+  hidden: {
+    rotate: -180,
+  },
+  visible: {
+    rotate: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+const pathVariant = {
+  hidden: { opacity: 0, pathLength: 0 },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    transition: { ease: "easeInOut", duration: 2 },
+  },
+};
+
 const Header = () => {
   return (
     <header>
       <div className="logo">
-        <svg
+        <motion.svg
+          variants={svgVariant}
+          initial="hidden"
+          animate="visible"
           className="pizza-svg"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 100 100"
         >
-          <path
+          <motion.path
+            variants={pathVariant}
             fill="none"
             d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
           />
-          <path fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" />
-        </svg>
+          <motion.path
+            variants={pathVariant}
+            fill="none"
+            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+          />
+        </motion.svg>
       </div>
       <motion.div
         className="title"
